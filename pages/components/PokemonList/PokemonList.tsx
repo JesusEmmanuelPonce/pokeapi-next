501e2bf5924cde015f17367d2f7fc4a6b0671cb3
JesusEmmanuelPonce <j.emmanuel.pb@hotmail.com>
2023-10-12 17:27:14 -0600

    feat: pokemon card

--- pages/components/PokemonList/PokemonList.tsx
+++ pages/components/PokemonList/PokemonList.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
 import { FC } from 'react';
 
 import { Pokemon } from '@/interfaces/pokemon.interface';
+import PokemonCard from '../PokemonCard';
 
 interface IPokemonListProps {
     pokemons: Pokemon[]
@@ -11,15 +11,7 @@ const PokemonList: FC<IPokemonListProps> = ({ pokemons }) => {
     return (
         <ul style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)" }}>
             {pokemons.map((pokemon =>
-                <li key={pokemon?.id}>
-                    <p>{pokemon?.name}</p>
-                    <Image
-                        src={pokemon?.image}
-                        width={80}
-                        height={80}
-                        alt="pokemon"
-                    />
-                </li>
+                <PokemonCard pokemon={pokemon} />
             ))}
         </ul>
     )
