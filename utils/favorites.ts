b1c0aa56455717e314ec9e72da05346dcf7c5454
JesusEmmanuelPonce <j.emmanuel.pb@hotmail.com>
2023-10-19 22:15:23 -0600

    feat: added favorites func

---
+++ utils/favorites.ts
@@ -0,0 +1,13 @@
+const favorites = (id: number) => {
+    let favorites: number[] = JSON.parse(localStorage.getItem('favorites') || '[]');
+
+    if (favorites.includes(id)) {
+        favorites.filter(pokeId => pokeId !== id);
+    } else {
+        favorites.push(id);
+    }
+
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+}
+
+export default favorites;
